6ea243a8d3726cb8d2fd5e1cf80bcf2d9b71c6df
Arthur <arthur.chesse@ynov.com>
2023-11-16 12:29:33 +0100

    Minicraft 3.0

--- script/script.js
+++ script/script.js
@@ -1,8 +1,10 @@
+const screenWidth = window.innerWidth; // Obtient la largeur de l'écran
+const targetX = screenWidth / 3; // Position x au tiers de l'écran
 const persopose = document.getElementById("persopose");
 const marcheImages = document.querySelectorAll(".marche img");
 const persomarche3 = document.getElementById("persomarche3"); 
 const persomarche4 = document.getElementById("persomarche4");
- 
+
 let isMoving = false;
 let currentFrame = 0;
 let currentFrame2 = 1;
@@ -55,7 +57,7 @@ function animateWalking() {
         currentFrame4 = (currentFrame4 + 1) % marcheImages.length;
         marcheImages[currentFrame4].style.opacity = 1; // Affichez la nouvelle image
 
-        setTimeout(animateWalking, 500);
+        setTimeout(animateWalking, 400);
     }
 }
 
@@ -82,7 +84,6 @@ window.addEventListener("keydown", (event) => {
             characterX = Math.min(container.offsetWidth - 100, characterX);
         }
         document.querySelector(".personnage").style.transform = `translateX(${characterX}px)`;
-        document.querySelector(".monde").scrollLeft += characterSpeed;
     }
 });
 
@@ -100,7 +101,51 @@ function startTapping() {
     if (!isTapping) {
         isTapping = true;
         persopose.style.display = "none"; // Cachez l'image de pose
+
+        // Ajout de la fonction jump
+        jump();
         animateTapping();
+    }
+}
+
+function jump() {
+    const jumpHeight = 100; // Ajustez la hauteur du saut selon vos besoins
+    const jumpDuration = 500; // Ajustez la durée du saut selon vos besoins
+
+    const character = document.querySelector(".personnage");
+    const characterInitialTop = character.getBoundingClientRect().top;
+
+    let startTime;
+
+    function jumpStep(timestamp) {
+        if (!startTime) {
+            startTime = timestamp;
+        }
+
+        const progress = timestamp - startTime;
+        const newY = characterInitialTop - easeInOutQuad(progress, 0, jumpHeight, jumpDuration);
+
+        character.style.top = `${newY}px`;
+
+        if (progress < jumpDuration) {
+            requestAnimationFrame(jumpStep);
+        } else {
+            // Fin du saut, réinitialiser la position et vérifier la collision
+            character.style.top = "0";
+            checkCollision();
+        }
+    }
+
+    requestAnimationFrame(jumpStep);
+}
+
+// Fonction d'interpolation pour le saut (EaseInOutQuad)
+function easeInOutQuad(t, b, c, d, isDescend = false) {
+    t /= d / 2;
+    if (t < 1) {
+        return isDescend
+            ? (c / 4) * t * t + b  // Division par 4 pour ajuster la descente
+            : (c / 2) * t * t + b;
     }
 }
 
@@ -117,7 +162,7 @@ function animateTapping() {
             } else {
                 animateTapping(); // Affichez la prochaine image
             }
-        }, 300); // Ajustez la durée d'affichage de chaque image (en millisecondes)
+        }, 190); // Ajustez la durée d'affichage de chaque image (en millisecondes)
     }
 }
 
@@ -126,8 +171,53 @@ function stopTapping() {
     persopose.style.display = "block"; // Affichez l'image de pose
 }
 
+
+
 window.addEventListener("keydown", (event) => {
     if (event.key === " " && !isTapping) { // Barre d'espace
         startTapping();
     }
 });
+
+const blocs = document.querySelectorAll(".bloc");
+const surprises = document.querySelectorAll(".surprise");
+
+function checkCollision() {
+    const characterRect = document.querySelector(".personnage").getBoundingClientRect();
+
+    blocs.forEach((bloc, index) => {
+        const blocRect = bloc.getBoundingClientRect();
+        const surprise = document.querySelector(`.surprise-${index + 1}`);
+        
+
+        // Vérifiez si le personnage frappe en dessous du bloc pendant le saut
+        const hitZoneBottom = blocRect.top + 10;
+        const hitZoneTop = blocRect.bottom - 10;
+
+        if (
+            (characterRect.bottom >= hitZoneTop && characterRect.bottom <= hitZoneBottom) ||
+            (isTapping && characterRect.bottom >= hitZoneTop && characterRect.top <= hitZoneBottom)
+        ) {
+            // Vérifiez si le personnage est proche de la position x cible
+            if (characterRect.left <= targetXFromLeft && characterRect.right >= targetXFromLeft) {
+                if (!surprise.classList.contains("activated")) {
+                    surprise.style.display = "block";
+                }
+            }
+        } else {
+            surprise.style.display = "none";
+            surprise.classList.remove("activated");
+        }
+    });
+}
+
+
+setInterval(checkCollision, 1);
+
+
+function animate() {
+    checkCollision();
+    requestAnimationFrame(animate);
+}
+
+animate();
